989a134c5f1314ba96b894f9a933499a2f1a754c
terence tsao <terence@prysmaticlabs.com>
2021-06-05 11:52:55 -0700

    Add bitvector32 and fix constant length return (#45)

--- bitvector32.go
+++ bitvector32.go
@@ -1,25 +1,24 @@
 package bitfield
 
 import (
-	"encoding/binary"
 	"math/bits"
 )
 
-var _ = Bitfield(Bitvector64{})
+var _ = Bitfield(Bitvector32{})
 
-// Bitvector64 is a bitfield with a fixed defined size of 64. There is no length bit
+// Bitvector32 is a bitfield with a fixed defined size of 32. There is no length bit
 // present in the underlying byte array.
-type Bitvector64 []byte
+type Bitvector32 []byte
 
-// NewBitvector64 creates a new bitvector of size 64.
-func NewBitvector64() Bitvector64 {
-	byteArray := [8]byte{}
+// NewBitvector32 creates a new bitvector of size 32.
+func NewBitvector32() Bitvector32 {
+	byteArray := [4]byte{}
 	return byteArray[:]
 }
 
 // BitAt returns the bit value at the given index. If the index requested
 // exceeds the number of bits in the bitvector, then this method returns false.
-func (b Bitvector64) BitAt(idx uint64) bool {
+func (b Bitvector32) BitAt(idx uint64) bool {
 	// Out of bounds, must be false.
 	if idx >= b.Len() {
 		return false
@@ -32,7 +31,7 @@ func (b Bitvector64) BitAt(idx uint64) bool {
 // SetBitAt will set the bit at the given index to the given value. If the index
 // requested exceeds the number of bits in the bitvector, then this method returns
 // false.
-func (b Bitvector64) SetBitAt(idx uint64, val bool) {
+func (b Bitvector32) SetBitAt(idx uint64, val bool) {
 	// Out of bounds, do nothing.
 	if idx >= b.Len() {
 		return
@@ -47,12 +46,12 @@ func (b Bitvector64) SetBitAt(idx uint64, val bool) {
 }
 
 // Len returns the number of bits in the bitvector.
-func (b Bitvector64) Len() uint64 {
-	return 64
+func (b Bitvector32) Len() uint64 {
+	return 32
 }
 
 // Count returns the number of 1s in the bitvector.
-func (b Bitvector64) Count() uint64 {
+func (b Bitvector32) Count() uint64 {
 	if len(b) == 0 {
 		return 0
 	}
@@ -63,9 +62,9 @@ func (b Bitvector64) Count() uint64 {
 	return uint64(c)
 }
 
-// Bytes returns the bytes data representing the bitvector64. This method
+// Bytes returns the bytes data representing the bitvector32. This method
 // bitmasks the underlying data to ensure that it is an accurate representation.
-func (b Bitvector64) Bytes() []byte {
+func (b Bitvector32) Bytes() []byte {
 	if len(b) == 0 {
 		return []byte{}
 	}
@@ -74,31 +73,8 @@ func (b Bitvector64) Bytes() []byte {
 	return ret[:]
 }
 
-// Shift bitvector by i. If i >= 0, perform left shift, otherwise right shift.
-func (b Bitvector64) Shift(i int) {
-	if len(b) == 0 {
-		return
-	}
-
-	// Shifting greater than 64 bits is pointless and can have unexpected behavior.
-	if i > 64 {
-		i = 64
-	} else if i < -64 {
-		i = -64
-	}
-	if i >= 0 {
-		num := binary.BigEndian.Uint64(b)
-		num <<= uint8(i)
-		binary.BigEndian.PutUint64(b, num)
-	} else {
-		num := binary.BigEndian.Uint64(b)
-		num >>= uint8(i * -1)
-		binary.BigEndian.PutUint64(b, num)
-	}
-}
-
-func (b Bitvector64) BitIndices() []int {
-	indices := make([]int, 0, 64)
+func (b Bitvector32) BitIndices() []int {
+	indices := make([]int, 0, 32)
 	for i, bt := range b {
 		for j := 0; j < 8; j++ {
 			bit := byte(1 << uint(j))
