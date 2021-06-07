4da71aaf6c2df60349036b586f5fde377fb00ee0
terence tsao <terence@prysmaticlabs.com>
2021-06-07 13:00:45 -0700

    Support for bitvector128 (#49)

---
+++ bitvector128.go
@@ -0,0 +1,122 @@
+package bitfield
+
+import (
+	"encoding/binary"
+	"math/bits"
+)
+
+var _ = Bitfield(Bitvector128{})
+
+// Bitvector128 is a bitfield with a fixed defined size of 128. There is no length bit
+// present in the underlying byte array.
+type Bitvector128 []byte
+
+const bitvector128ByteSize = 16
+const bitvector128BitSize = bitvector128ByteSize * 8
+
+// NewBitvector128 creates a new bitvector of size 128.
+func NewBitvector128() Bitvector128 {
+	byteArray := [bitvector128ByteSize]byte{}
+	return byteArray[:]
+}
+
+// BitAt returns the bit value at the given index. If the index requested
+// exceeds the number of bits in the bitvector, then this method returns false.
+func (b Bitvector128) BitAt(idx uint64) bool {
+	// Out of bounds, must be false.
+	if idx >= b.Len() {
+		return false
+	}
+
+	i := uint8(1 << (idx % 8))
+	return b[idx/8]&i == i
+}
+
+// SetBitAt will set the bit at the given index to the given value. If the index
+// requested exceeds the number of bits in the bitvector, then this method returns
+// false.
+func (b Bitvector128) SetBitAt(idx uint64, val bool) {
+	// Out of bounds, do nothing.
+	if idx >= b.Len() {
+		return
+	}
+
+	bit := uint8(1 << (idx % 8))
+	if val {
+		b[idx/8] |= bit
+	} else {
+		b[idx/8] &^= bit
+	}
+}
+
+// Len returns the number of bits in the bitvector.
+func (b Bitvector128) Len() uint64 {
+	return bitvector128BitSize
+}
+
+// Count returns the number of 1s in the bitvector.
+func (b Bitvector128) Count() uint64 {
+	if len(b) == 0 {
+		return 0
+	}
+	c := 0
+	for i, bt := range b {
+		if i >= bitvector128ByteSize {
+			break
+		}
+		c += bits.OnesCount8(bt)
+	}
+	return uint64(c)
+}
+
+// Bytes returns the bytes data representing the Bitvector128.
+func (b Bitvector128) Bytes() []byte {
+	if len(b) == 0 {
+		return []byte{}
+	}
+	ln := min(len(b), bitvector128ByteSize)
+	ret := make([]byte, ln)
+	copy(ret, b[:ln])
+	return ret[:]
+}
+
+// Shift bitvector by i. If i >= 0, perform left shift, otherwise right shift.
+func (b Bitvector128) Shift(i int) {
+	if len(b) == 0 {
+		return
+	}
+
+	// Shifting greater than 128 bits is pointless and can have unexpected behavior.
+	if i > bitvector128BitSize {
+		i = bitvector128BitSize
+	} else if i < -bitvector128BitSize {
+		i = -bitvector128BitSize
+	}
+	if i >= 0 {
+		num := binary.BigEndian.Uint64(b)
+		num <<= uint8(i)
+		binary.BigEndian.PutUint64(b, num)
+	} else {
+		num := binary.BigEndian.Uint64(b)
+		num >>= uint8(i * -1)
+		binary.BigEndian.PutUint64(b, num)
+	}
+}
+
+// BitIndices returns the list of indices that are set to 1.
+func (b Bitvector128) BitIndices() []int {
+	indices := make([]int, 0, bitvector128BitSize)
+	for i, bt := range b {
+		if i >= bitvector128ByteSize {
+			break
+		}
+		for j := 0; j < 8; j++ {
+			bit := byte(1 << uint(j))
+			if bt&bit == bit {
+				indices = append(indices, i*8+j)
+			}
+		}
+	}
+
+	return indices
+}
