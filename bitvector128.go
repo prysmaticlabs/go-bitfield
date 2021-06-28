d347afd4e73d8e56098e3c837a1061f373e454c4
terence tsao <terence@prysmaticlabs.com>
2021-06-28 11:25:38 -0700

    Add contains for bit vector 128 (#50)
    * Add contains
    
    * Bitvector32
    
    * Dont need to change bitvector32
    
    * Dont need to change bitvector512

--- bitvector128.go
+++ bitvector128.go
@@ -120,3 +120,22 @@ func (b Bitvector128) BitIndices() []int {
 
 	return indices
 }
+
+// Contains returns true if the bitlist contains all of the bits from the provided argument
+// bitlist. This method will panic if bitlists are not the same length.
+func (b Bitvector128) Contains(c Bitvector128) bool {
+	if b.Len() != c.Len() {
+		panic("bitvector are different lengths")
+	}
+
+	// To ensure all of the bits in c are present in b, we iterate over every byte, combine
+	// the byte from b and c, then XOR them against b. If the result of this is non-zero, then we
+	// are assured that a byte in c had bits not present in b.
+	for i := 0; i < len(b); i++ {
+		if b[i]^(b[i]|c[i]) != 0 {
+			return false
+		}
+	}
+
+	return true
+}
